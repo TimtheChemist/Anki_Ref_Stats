65b539588259e11ff73f849b0165b94e92ff097b
TimtheChemist <timothy.kwok.e@gmail.com>
2026-01-25 19:19:18 +0000

    Added f-strings for Streamlit sub-headers; added page directory and Stats.py; renamed main.py to Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,4 @@
-from stats_functions import parse_tags, get_tag_counts, tag_dict_organiser
 from core import generate_paper_frequencies, generate_textbook_frequencies, get_textbooks_by_note_range, get_papers_by_note_range, convert_string_to_df
-
 import os
 import streamlit as st
 
@@ -30,21 +28,21 @@ if nontarget_tags == ['']:
 
 # Buttons to trigger functions
 if st.sidebar.button("Generate Paper Frequencies"):
-    st.subheader("Top Paper Frequencies")
+    st.subheader(f"Showing the Top {range_of_references[0]} to {range_of_references[1]} Papers")
     reference_df = convert_string_to_df(generate_paper_frequencies(full_path, range_of_references, target_tags=target_tags, nontarget_tags=nontarget_tags))
     st.dataframe(reference_df, hide_index=True)
 
 if st.sidebar.button("Generate Textbook Frequencies"):
-    st.subheader("Top Textbook Frequencies")
+    st.subheader(f"Showing the Top {range_of_references[0]} to {range_of_references[1]} Textbooks")
     reference_df = convert_string_to_df(generate_textbook_frequencies(full_path, range_of_references, target_tags=target_tags, nontarget_tags=nontarget_tags))
     st.dataframe(reference_df, hide_index=True)
 
 if st.sidebar.button("Get Papers by Note Range"):
-    st.subheader("Papers by Note Range")
+    st.subheader(f"Showing Papers that Appear in {range_of_notes[0]} to {range_of_notes[1]} Notes")
     reference_df = convert_string_to_df(get_papers_by_note_range(full_path, range_of_notes, target_tags=target_tags, nontarget_tags=nontarget_tags))
     st.dataframe(reference_df, hide_index=True)
 
 if st.sidebar.button("Get Textbooks by Note Range"):
-    st.subheader("Textbooks by Note Range")
+    st.subheader(f"Showing Textbooks that Appear in {range_of_notes[0]} to {range_of_notes[1]} Notes")
     reference_df = convert_string_to_df(get_textbooks_by_note_range(full_path, range_of_notes, target_tags=target_tags, nontarget_tags=nontarget_tags))
     st.dataframe(reference_df, hide_index=True)
---
+++ pages/Stats.py
@@ -0,0 +1,5 @@
+from stats_functions import parse_tags, get_tag_counts, tag_dict_organiser
+from core import generate_paper_frequencies, generate_textbook_frequencies, get_textbooks_by_note_range, get_papers_by_note_range, convert_string_to_df
+
+import os
+import streamlit as st
