43477ba48ce955b12880e5b2bb3fdc062075a780
TimtheChemist <timothy.kwok.e@gmail.com>
2026-01-25 11:33:32 +0000

    Re-worded the streamlit input messages

--- gui.py
+++ gui.py
@@ -11,13 +11,13 @@ st.title("Anki Reference Parser")
 # Input fields for user
 st.sidebar.header("Input Parameters")
 path = st.sidebar.text_input("Enter the file path:", r'"/home/timot/workspace/github.com/Anki_Ref_Stats"')
-filename = st.sidebar.text_input("Enter the plaintext file name (without extension):", 'All_Decks_Cards.txt')
-full_path = os.path.join(path.strip('"'), filename)
+filename = st.sidebar.text_input("Enter the plaintext file name (without extension):", 'All_Decks_Cards')
+full_path = os.path.join(path.strip('"'), filename + ".txt")
 
-range_of_references = st.sidebar.text_input("Enter the range of references (by occurrence): ", "1, 20").split(",")
+range_of_references = st.sidebar.text_input("Enter the range of references (e.g., enter '1, 20' if you want to see the 1st to 20th most frequently cited references): ", "1, 20").split(",")
 range_of_references = (int(range_of_references[0]), int(range_of_references[1]))
 
-range_of_notes = st.sidebar.text_input("Enter the range of notes for filtering textbooks/papers: ", "10, 30").split(",")
+range_of_notes = st.sidebar.text_input("Enter the range of notes for filtering references (e.g., enter '10, 30' if you want to see all references that may be found in 10 to 30 notes): ", "10, 30").split(",")
 range_of_notes = (int(range_of_notes[0]), int(range_of_notes[1]))
 
 target_tags = st.sidebar.text_input("Enter the tags for matching: ", "Review").split(",")
